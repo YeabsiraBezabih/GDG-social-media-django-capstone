266cacefcba5f2adb0c11ae496e38aa4c89dd424
naty-pro <natnaelasfaw16@gmail.com>
2025-04-05 21:25:06 +0300

    Add like and comment interaction features

---
+++ api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Post, Like, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'content', 'timestamp', 'updated', 'likes_count', 'comments_count']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'timestamp']
+        read_only_fields = ['user', 'timestamp']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'content', 'timestamp']
+        read_only_fields = ['user', 'timestamp']
